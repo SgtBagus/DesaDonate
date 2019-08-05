9fb4ab4218a3e9dbbef89dce249b27f32a9f8717
SgtBagus <procw57@gmail.com>
2019-08-05 09:54:49 +0700

    Versi 1.0

---
+++ application/controllers/master/Titleku.php
@@ -0,0 +1,99 @@
+
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+	class Titleku extends MY_Controller {
+
+		public function __construct()
+		{
+			parent::__construct();
+		}
+
+		public function index()
+		{
+			$data['page_name'] = "titleku";
+			$this->template->load('template/template','master/titleku/all-titleku',$data);
+		}
+
+		public function create()
+		{
+			$data['page_name'] = "titleku";
+			$this->template->load('template/template','master/titleku/add-titleku',$data);
+		}
+
+
+		public function validate()
+		{
+			$this->form_validation->set_error_delimiters('<li>', '</li>');
+	$this->form_validation->set_rules('dt[title]', '<strong>Title</strong>', 'required');
+	}
+
+		public function store()
+		{
+			$this->validate();
+	    	if ($this->form_validation->run() == FALSE){
+				$this->alert->alertdanger(validation_errors());     
+	        }else{
+	        	$dt = $_POST['dt'];
+				$dt['created_at'] = date('Y-m-d H:i:s');
+				$dt['status'] = "ENABLE";
+				$str = $this->db->insert('titleku', $dt);
+				$last_id = $this->db->insert_id();$this->alert->alertsuccess('Success Send Data');   
+					
+			}
+		}
+
+		public function json()
+		{
+			$status = $_GET['status'];
+			if($status==''){
+				$status = 'ENABLE';
+			}
+			header('Content-Type: application/json');
+	        $this->datatables->select('id,title,status');
+	        $this->datatables->where('status',$status);
+	        $this->datatables->from('titleku');
+	        if($status=="ENABLE"){
+	        $this->datatables->add_column('view', '<div class="btn-group"><button type="button" class="btn btn-sm btn-primary" onclick="edit($1)"><i class="fa fa-pencil"></i> Edit</button></div>', 'id');
+
+	    	}else{
+	        $this->datatables->add_column('view', '<div class="btn-group"><button type="button" class="btn btn-sm btn-primary" onclick="edit($1)"><i class="fa fa-pencil"></i> Edit</button><button type="button" onclick="hapus($1)" class="btn btn-sm btn-danger"><i class="fa fa-trash-o"></i> Hapus</button></div>', 'id');
+
+	    	}
+	        echo $this->datatables->generate();
+		}
+		public function edit($id)
+		{
+			$data['titleku'] = $this->mymodel->selectDataone('titleku',array('id'=>$id));$data['page_name'] = "titleku";
+			$this->template->load('template/template','master/titleku/edit-titleku',$data);
+		}
+
+		public function update()
+		{
+			$this->form_validation->set_error_delimiters('<li>', '</li>');
+			
+			$this->validate();
+			
+
+	    	if ($this->form_validation->run() == FALSE){
+				$this->alert->alertdanger(validation_errors());     
+	        }else{
+				$id = $this->input->post('id', TRUE);		$dt = $_POST['dt'];
+					$dt['updated_at'] = date("Y-m-d H:i:s");
+					$this->mymodel->updateData('titleku', $dt , array('id'=>$id));
+					$this->alert->alertsuccess('Success Update Data');  }
+		}
+
+		public function delete()
+		{
+				$id = $this->input->post('id', TRUE);$this->alert->alertdanger('Success Delete Data');     
+		}
+
+		public function status($id,$status)
+		{
+			$this->mymodel->updateData('titleku',array('status'=>$status),array('id'=>$id));
+			redirect('master/Titleku');
+		}
+
+
+	}
+?>
